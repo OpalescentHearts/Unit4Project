4a11eb0040903c1ae8f8681ae431a80aaf057ab2
OpalescentHearts <Amber38871@gmail.com>
2022-09-29 08:58:30 -0500

    ok

--- app/views.py
+++ app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+def project_view(request, flower, month):
+    context = {
+        "flower": flower,
+        "month": month,
+    }
+    return render(request, "project.html", context)
